7e68e58424d5e3a23dfadcafdcc18d936026992e
Kevin Jose da Silva Monteiro <kjosedasilvamonteiro1@gmail.com>
2026-09-21 22:44:19 -0300

    adiciona rotas sobre e tarefas

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ app = Flask (__name__)
 @app.route('/')
 def home():
     return 'Olá, Mundo!'
+
+@app.route('/sobre')
+def sobre():
+    return 'Esta é a pagina sobre o projeto.'
+
+@app.route('/tarefas')
+def tarefas():
+    return 'Aqui vão aparecer mais tarefas.'
+
 if __name__=='__main__':
     app.run(debug=True)
 
